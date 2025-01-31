526b62766d381e88223554be2e53cecee5abe9e3
MAziz-6 <matt.aziz6@gmail.com>
2025-01-31 12:16:01 -0800

    Added character count feature

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ def get_word_count(file_contents):
         count += 1
     print(count)
 
+def get_char_count(file_contents):
+    char_count = {}
+    for char in file_contents:
+        char = char.lower()
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+
+    
 
 
 def main():
@@ -12,7 +23,9 @@ def main():
         file_contents = f.read()
         # print(file_contents)
     # return file_contents
-
-    get_word_count(file_contents)
+    # get_word_count(file_contents)
+    char_count = get_char_count(file_contents)
+    print(char_count)
+    
 
 main()
